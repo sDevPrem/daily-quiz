200ec774ec94f6f6f3851d888632d392a7a0e4cf
Prem Thakur <sdevpremthakur@gmail.com>
2023-05-18 13:39:27 +0530

    Set up drawer

--- app/src/main/java/com/sdevprem/dailyquiz/ui/HomeFragment.kt
+++ app/src/main/java/com/sdevprem/dailyquiz/ui/HomeFragment.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.sdevprem.dailyquiz.R
 import com.sdevprem.dailyquiz.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(){
@@ -22,6 +27,15 @@ class HomeFragment : Fragment(){
                 false
             ).apply { binding = this }
             .root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val configuration = AppBarConfiguration(
+            setOf(R.id.homeFragment),//top level destinations
+            binding.drawerLayout
+        )
+        binding.toolbar.setupWithNavController(findNavController(),configuration)
     }
 
 }
