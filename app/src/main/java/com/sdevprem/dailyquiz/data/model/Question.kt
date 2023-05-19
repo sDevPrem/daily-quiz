fdcf09100823282700c4885c5043a067f186ac7d
Prem Thakur <sdevpremthakur@gmail.com>
2023-05-19 11:52:21 +0530

    Change answer field type of question

--- app/src/main/java/com/sdevprem/dailyquiz/data/model/Question.kt
+++ app/src/main/java/com/sdevprem/dailyquiz/data/model/Question.kt
@@ -1,11 +1,11 @@
 package com.sdevprem.dailyquiz.data.model
 
 data class Question(
-    var description : String = "",
-    var opt1 : String = "",
-    var opt2 : String = "",
-    var opt3 : String = "",
-    var opt4 : String = "",
-    var answer : String = "",
+    var description: String = "",
+    var opt1: String = "",
+    var opt2: String = "",
+    var opt3: String = "",
+    var opt4: String = "",
+    var answer: Int = -1, //will be given by network
     var userAnswer: Int = -1 //no option selected
 )
