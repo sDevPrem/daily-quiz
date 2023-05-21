d280489398ac65007b9e2ed370adb917a8a263b2
Prem Thakur <sdevpremthakur@gmail.com>
2023-05-21 09:53:28 +0530

    Add timestamp to quiz pojo!
    1. Add firebase timestamp to Quiz object.
    2. Now title of the quiz will be calculated according to the timestamp

--- app/src/main/java/com/sdevprem/dailyquiz/data/model/Quiz.kt
+++ app/src/main/java/com/sdevprem/dailyquiz/data/model/Quiz.kt
@@ -1,10 +1,25 @@
 package com.sdevprem.dailyquiz.data.model
 
+import android.icu.text.SimpleDateFormat
+import android.os.Build
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.Exclude
+import java.util.Locale
 
 data class Quiz(
     @DocumentId
     var id: String = "",
-    var title: String = "",
-    var questions: MutableMap<String, Question> = mutableMapOf()
-)
+    var questions: MutableMap<String, Question> = mutableMapOf(),
+    var timestamp: Timestamp = Timestamp.now()
+) {
+    private val titleFormatter = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+    } else {
+        java.text.SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+    }
+
+    @get:Exclude
+    val title: String
+        get() = titleFormatter.format(timestamp.toDate())
+}
